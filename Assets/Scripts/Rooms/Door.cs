680a9cf002435e4808650d97814884f19ed1dffa
Bartosz <barth.webdesign@gmail.com>
2024-07-01 21:21:28 +0200

    [Feat]: last door handle logic

--- Assets/Scripts/Rooms/Door.cs
+++ Assets/Scripts/Rooms/Door.cs
@@ -1,10 +1,16 @@
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class Door : MonoBehaviour
 {
     [SerializeField] private Transform previousRoom;
     [SerializeField] private Transform nextRoom;
     [SerializeField] private CameraController cam;
+
+    [Header("Last doors handle")] [SerializeField]
+    private int nextLevelIndex;
+
+    [SerializeField] private int menuSceneIndex;
 
     private void Awake()
     {
@@ -17,16 +23,34 @@ public class Door : MonoBehaviour
         {
             if (collision.transform.position.x < transform.position.x)
             {
-                cam.MoveToNewRoom(nextRoom);
-                nextRoom.GetComponent<Room>().ActivateRoom(true);
-                previousRoom.GetComponent<Room>().ActivateRoom(false);
+                if (nextRoom != null)
+                {
+                    cam.MoveToNewRoom(nextRoom);
+                    nextRoom.GetComponent<Room>().ActivateRoom(true);
+                    previousRoom.GetComponent<Room>().ActivateRoom(false);
+                }
+                else
+                    LoadNextScene();
             }
             else
             {
                 cam.MoveToNewRoom(previousRoom);
                 previousRoom.GetComponent<Room>().ActivateRoom(true);
-                nextRoom.GetComponent<Room>().ActivateRoom(false);
+                if (nextRoom != null)
+                    nextRoom.GetComponent<Room>().ActivateRoom(false);
             }
+        }
+    }
+
+    private void LoadNextScene()
+    {
+        if (nextLevelIndex >= 0)
+        {
+            SceneManager.LoadScene(nextLevelIndex, LoadSceneMode.Single);
+        }
+        else
+        {
+            SceneManager.LoadScene(menuSceneIndex, LoadSceneMode.Single);
         }
     }
 }
